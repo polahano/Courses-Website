b6b0217b48fae79b6ff20797ea85abd3b87ff9ce
Pola Hano <pola.hano@gmail.com>
2025-09-10 02:09:48 +0300

    banner-dots

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,7 +4,6 @@ import { GalleryService } from '../../core/services/gallery/gallery.service';
 import { CourseCardComponent } from "../../shared/components/course-card/course-card.component";
 import { ICourse } from '../../shared/interfaces/icourse';
 import { CoursesService } from '../../core/services/courses/courses.service';
-import { BannerComponent } from "../../shared/components/banner/banner.component";
 import { NgxNumberTickerComponent } from '@omnedia/ngx-number-ticker';
 import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
@@ -15,7 +14,7 @@ import { DeviceService } from '../../core/services/device/device.service';
 
 @Component({
   selector: 'app-home',
-  imports: [CourseCardComponent, BannerComponent, NgxNumberTickerComponent, CarouselModule, NgbNavModule],
+  imports: [CourseCardComponent, NgxNumberTickerComponent, CarouselModule, NgbNavModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -24,24 +23,11 @@ export class HomeComponent implements OnInit {
   isMobile: boolean = false;
 
 
-  // constructor() {
-  //   if(isPlatformBrowser(this.platformID)){
-  //     this.deviceService.isMobileDevice()
-  //   window.addEventListener('resize', () => this.deviceService.isMobileDevice());
-  //   }
-
-  // }
-
-  // checkDevice() {
-  //   this.isMobile = window.innerWidth <= 599; // breakpoint for mobile
-  // }
 
   readonly deviceService = inject(DeviceService)
   private readonly galleryService = inject(GalleryService)
   private readonly coursesService = inject(CoursesService)
-  // readonly deviceService = inject(DeviceService)
 
-  // isMobile = this.deviceService.isMobile
   platformID = inject(PLATFORM_ID);
   isBrowser = isPlatformBrowser(this.platformID);
   sliderImages: ISlider[] = [];
@@ -53,8 +39,6 @@ export class HomeComponent implements OnInit {
 
     if (this.isBrowser) {
       this.deviceService.isMobileDevice();
-
-      // Handle resize safely
       window.addEventListener('resize', () => this.deviceService.isMobileDevice());
     }
 
@@ -77,7 +61,7 @@ export class HomeComponent implements OnInit {
     })
   }
 
-  customOptions: OwlOptions = {
+  galleryCustomOptions: OwlOptions = {
     loop: true,
     mouseDrag: true,
     touchDrag: true,
@@ -101,6 +85,57 @@ export class HomeComponent implements OnInit {
     },
     nav: true
   }
+  bannerCustomOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    freeDrag: true,
+    dots: true,
+    navSpeed: 700,
+    autoWidth: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1200: {
+        items: 1
+      }
+    },
+  }
+
+  banners = [
+    {
+      "id": 0,
+      "brief": "Learn UI-UX Design skills with weekend UX . The latest online learning system and material that help your knowledge growing.",
+      "image": 'images/hero-lg.png',
+      "order": 1,
+      "title": "\u003Ch1\u003EBetter \u003Cspan\u003EInsights\u003C/span\u003E For Business \u003Cspan\u003EGrowth\u003C/span\u003E\u003C/h1\u003E",
+      "category": "Outsource",
+      "colorCode": "00B8FF"
+    },
+    {
+      "id": 1,
+      "brief": "Whether you’re a startup or an enterprise, our creative team brings your vision to life with innovative designs and robust development.",
+      "image": 'images/hero-lg-2.png',
+      "order": 3,
+      "title": "\u003Ch1\u003ETransforming \u003Cspan\u003EIdeas\u003C/span\u003E Into Digital \u003Cspan\u003EReality\u003C/span\u003E\u003C/h1\u003E",
+      "category": "Marking",
+      "colorCode": "A6E51C"
+    },
+    {
+      "id": 2,
+      "brief": "Beyond development, we’re here to ensure your success with continuous support, optimization, and a commitment to your growth.",
+      "image": 'images/hero-lg-3.png',
+      "order": 2,
+      "title": "\u003Ch1\u003EDedicated \u003Cspan\u003ESupport\u003C/span\u003E & Continuous \u003Cspan\u003EGrowth\u003C/span\u003E\u003C/h1\u003E",
+      "category": "Trendy Inside-Out",
+      "colorCode": "FD6F00"
+    }
+  ]
 
   filterCourses(category: string): ICourse[] {
     if (category === 'all') {
